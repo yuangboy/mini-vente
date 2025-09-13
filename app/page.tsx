dddbc7c11a18fdd573e841f5f45687551090ce87
Albin_Mp <albinmpobo@gmail.com>
2025-09-13 22:24:24 +0200

    add shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
 import Image from "next/image";
 
+import { ModeToggle } from "../src/components/ModeToggle";
+
 export default function Home() {
   return (
+
+   
+
+
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+       <ModeToggle />
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
